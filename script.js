c5c10acf42e1911a3152e13bbc06812bce87f1b9
Adam Hartman <adamhartman5@gmail.com>
2021-08-01 00:19:37 -0400

    Create get items function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getItems(){
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+    });
+}
