25b01a0201beb4284a24796b95cc0c9208435813
Adam Hartman <adamhartman5@gmail.com>
2021-08-01 23:03:08 -0400

    Fix firebase config and create items function

--- firebase.js
+++ firebase.js
@@ -1,15 +1,14 @@
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyDk5oh77cxGbpMpI5huGj3bmIf_pSoVhks",
-    authDomain: "clone-two-39d03.firebaseapp.com",
-    projectId: "clone-two-39d03",
-    storageBucket: "clone-two-39d03.appspot.com",
-    messagingSenderId: "378610183471",
-    appId: "1:378610183471:web:2a19630c689e6f7bbe05da",
-    measurementId: "G-98LW7GBHTF"
-  };
-// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyDk5oh77cxGbpMpI5huGj3bmIf_pSoVhks",
+  authDomain: "clone-two-39d03.firebaseapp.com",
+  projectId: "clone-two-39d03",
+  storageBucket: "clone-two-39d03.appspot.com",
+  messagingSenderId: "378610183471",
+  appId: "1:378610183471:web:2a19630c689e6f7bbe05da",
+  measurementId: "G-98LW7GBHTF"
+};
+
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function getItems(){
     db.collection("items").get().then((querySnapshot) => {
+        let items = [];
         querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
+            items.push({
+                id: doc.id,
+                image: doc.data().image,
+                name: doc.data().name,
+                brand: doc.data().brand,
+                rating: doc.data().rating,
+                price: doc.data().price,
+            })
         });
+        generateItems(items);
     });
 }
+
+function generateItems(items) {
+    items.forEach((item) => {
+        
+    })
+}
+
+getItems();
