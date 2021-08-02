1ac7b77cfdda0c0fa15479c3ba2ee926205074b5
Adam Hartman <adamhartman5@gmail.com>
2021-08-01 23:15:49 -0400

    Pull products from firebase

--- script.js
+++ script.js
@@ -16,9 +16,32 @@ function getItems(){
 }
 
 function generateItems(items) {
+    let itemsHTML = "";
     items.forEach((item) => {
-        
+        itemsHTML += `
+            <div class="main-product mr-5">
+                <div class="product-image w-48 h-52 bg-white rounded-lg">
+                    <img class="w-full h-full object-contain p-4" src="${item.image}">
+                </div>
+                <div class="product-name text-blue-400 font-bold mt-2 text-sm">
+                    ${item.name}
+                </div>
+                <div class="product-make text-gray-50 font-bold">
+                    ${item.brand}
+                </div>
+                <div class="product-rating text-yellow-500 my-1">
+                    ⭐⭐⭐⭐⭐ ${item.rating}
+                </div>
+                <div class="product-price font-bold text-blue-400 text-lg">
+                    $${item.price}
+                </div>
+                <div class="add-to-cart h-8 w-28 bg-yellow-500 flex items-center justify-center text-gray-700 rounded text-md cursor-pointer hover:bg-yellow-600">
+                    Add to cart
+                </div>
+            </div>`
     })
+
+    document.querySelector(".main-section-products").innerHTML = itemsHTML;
 }
 
 getItems();
